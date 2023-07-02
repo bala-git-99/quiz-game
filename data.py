01df4ea90f16c948ac7ab4c1671d6b874ce3ecc2
bala <balagrt1099@gmail.com>
2023-07-02 18:38:53 +0530

    Initial commit: a text based quiz game

---
+++ data.py
@@ -0,0 +1,39 @@
+question_data = [
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The movie &quot;The Nightmare before Christmas&quot; was all done with physical objects.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom "
+                 "Anderson.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Samuel L. Jackson had the words, &#039;Bad Motherf*cker&#039; in-scripted on his lightsaber during "
+                 "the filming of Star Wars.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "In Alfred Hitchcock&#039;s film &#039;Psycho&#039; it is said he used chocolate syrup to simulate "
+                 "the blood in the famous shower scene from ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "George Lucas directed the entire original Star Wars trilogy.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                                      "question": "&quot;Minions&quot; was released on the June 10th, 2015.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+]
